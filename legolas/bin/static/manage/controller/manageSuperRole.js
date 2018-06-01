1099e26dd64ece27c2f93289b631b8d1afd1f871
legolas-lee <legolas_lee@icloud.com>
2018-06-01 22:57:29 +0800

    md use

--- legolas/bin/static/manage/controller/manageSuperRole.js
+++ legolas/bin/static/manage/controller/manageSuperRole.js
@@ -2,7 +2,7 @@
  * Created by kreta on 2018/5/23.
  */
 //超级管理-角色管理
-angular.module('myApp',['ngMaterial']).controller('manageSuperRole',function ($scope,$http) {
+angular.module('myApp',['ngMaterial']).controller('manageSuperRole',function ($scope,$http,$mdDialog) {
     $scope.$emit('navState', {parrent:'manageSuper',current:'manageSuperRole'});
     function refreshList() {
         $http.get(transUrl('Accounts/getRoleList'),{params:{}})
@@ -29,4 +29,20 @@ angular.module('myApp',['ngMaterial']).controller('manageSuperRole',function ($s
         //$scope.modelAddRole=true;
     }
 
+    $scope.status = '  ';
+    $scope.customFullscreen = false;
+    $scope.showAdvanced = function(ev) {
+        $mdDialog.show({
+            templateUrl: './view/manageSuper/dialog1.tmpl.html',
+            parent: angular.element('.page-main'),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+    };
 })
